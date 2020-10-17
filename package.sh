4b880e0c4c998bd0f8ec20e179a5b88bdc77fc1d
Sergio Callegari <sergio.callegari@gmail.com>
2020-10-17 20:26:47 +0200

    Update packaging script

--- package.sh
+++ package.sh
@@ -1,13 +1,67 @@
 #!/bin/bash
 
 PKGNAME=everypage
-PKGVERS=2-0
-PKGCNT=("everypage.dtx" "everypage.ins" "README.md")
+PKGVERS=2-0-b
+PKGCNT=("README.md")
+TESTFILES=( )
+TESTENGINES=("pdflatex" "lualatex") 
 
-mkdir -p buildpkg/"$PKGNAME"_"$PKGVERS"
-cp "${PKGCNT[@]}" buildpkg/"$PKGNAME"_"$PKGVERS"
-cd buildpkg
-zip -r -9 "$PKGNAME"_"$PKGVERS".zip "$PKGNAME"_"$PKGVERS"
-rm -fr "$PKGNAME"_"$PKGVERS"
-cd ..
+build()
+  {
+    mkdir -p buildpkg/workdir
+    cp "$PKGNAME".dtx "$PKGNAME".ins "${PKGCNT[@]}" buildpkg/workdir/
+    cd buildpkg/workdir
+    cat > docstrip.cfg <<EOF
+\BaseDirectory{.}
+\UseTDS
+\askforoverwritefalse
+EOF
+    mkdir -p tex/latex/"$PKGNAME"
+    mkdir -p doc/latex/"$PKGNAME"/examples
+    tex "$PKGNAME".ins
+    lualatex "$PKGNAME".dtx
+    makeindex -s gind.ist "$PKGNAME"
+    makeindex -s gglo.ist -o "$PKGNAME".gls "$PKGNAME".glo
+    lualatex "$PKGNAME".dtx
+    lualatex "$PKGNAME".dtx
+    cp "$PKGNAME".pdf doc/latex/"$PKGNAME"/
+    cd -
+  }
+
+do_test()
+  {
+    mkdir -p buildpkg/testdir
+    cp buildpkg/workdir/tex/latex/"$PKGNAME"/"$PKGNAME".sty buildpkg/testdir
+    cp buildpkg/workdir/doc/latex/"$PKGNAME"/examples/* buildpkg/testdir
+    cd buildpkg/testdir
+    for name in "${TESTFILES[@]}"; do
+        for engine in "${TESTENGINES[@]}" ; do
+            $engine "$name"
+        done
+    done
+    cd -
+  }
+
+package()
+  {  
+    mkdir -p buildpkg/"$PKGNAME"_"$PKGVERS"
+    cp "$PKGNAME".dtx "$PKGNAME".ins "${PKGCNT[@]}" \
+       buildpkg/"$PKGNAME"_"$PKGVERS"/
+    cp buildpkg/workdir/"$PKGNAME".pdf buildpkg/"$PKGNAME"_"$PKGVERS"/
+    cd buildpkg
+    zip -r -9 "$PKGNAME"_"$PKGVERS".zip "$PKGNAME"_"$PKGVERS"
+    cd -
+  }
+
+clean()
+  {
+    cd buildpkg
+    rm -fr ./workdir ./testdir ./"$PKGNAME"_"$PKGVERS" ./"$PKGNAME".tds.zip
+    cd -
+  }
+    
+build
+do_test
+package
+clean
  
